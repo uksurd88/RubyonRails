5c822c156086cf33779411321fb8de3f73bf9720
Sukhdeep Singh <sukhdeepsingh@sukhdeepsingh.(none)>
2010-03-31 16:11:02 +0100

    Power of ror - tracks

---
+++ test/unit/tagging_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class TaggingTest < Test::Rails::TestCase
+  fixtures :taggings
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
